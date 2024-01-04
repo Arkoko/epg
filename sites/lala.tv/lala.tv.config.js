289c9fa165411015ff67b9fbc882e62b90502ab3
Arko <ryan0617@gmail.com>
2024-01-04 22:50:24 +0800

    fix time shift issue

--- sites/lala.tv/lala.tv.config.js
+++ sites/lala.tv/lala.tv.config.js
@@ -1,8 +1,15 @@
 const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
 
 module.exports = {
   site: 'lala.tv',
-  days: 5,
+  days: 1,
   lang: 'jp',
   delay: 5000,
 
@@ -25,16 +32,19 @@ module.exports = {
   parser: function (context, channel) {
     const programs = []
     const data = parseItems(context, channel)
-    const items = data.programs
+    const dates = Object.keys(data)
+    
     //console.log('parser date:'+date)
-    items.forEach(item => {
-      programs.push({
-        title: item.ps_episode_title,
-        start: parseStart(item, data.date),
-        stop: parseStop(item, data.date),
-        description: item.ps_episode_description,
-        icon: parseIcon(item),
-        sub_title: item.subtitle
+    dates.forEach(date => {
+      data[date].forEach(item =>{
+        programs.push({
+          title: item.ps_episode_title,
+          start: parseStart(item, date),
+          stop: parseStop(item, date),
+          description: item.ps_episode_description,
+          icon: parseIcon(item),
+          sub_title: item.subtitle
+        })
       })
     })
     //console.log(programs)
@@ -42,36 +52,20 @@ module.exports = {
   }
 }
 
-function parseItems({content, date}) {
+function parseItems({content}) {
   //console.log(channel)
-  if (content != '') {
-    const data = JSON.parse(content)
-    const dateStr = dayjs(date).format('YYYY-MM-DD')
-    //console.log(dateStr)
-    if(!data || !data[dateStr]){
-      return []
-    }
-  
-    return {'date': dateStr, 'programs':data[dateStr]}
-  } else {
+  if (!content || content == '')
     return []
-  }
+  
+  return  JSON.parse(content)  
 }
 
 function parseStart(item, date) {
-
   return getDayjsTime( item, 'ps_start_time', date)
 }
 
 function parseStop(item, date) {
-  //let starthr = parseInt(item['ps_start_time'].split(':')[0])
-  let endhr = parseInt(item['ps_end_time'].split(':')[0])
-  let enddate = date
-  if(endhr > 23){
-    enddate = date +1
-  }
-  //console.log('start: '+starthr + ' end: '+ endhr+ ' | '+enddate)
-  return getDayjsTime( item, 'ps_end_time', enddate)
+  return getDayjsTime( item, 'ps_end_time', date)
 }
 
 function parseIcon(item) {
@@ -79,14 +73,14 @@ function parseIcon(item) {
 }
 
 function getDayjsTime(item, attribute, date){
-  let starthr = parseInt(item[attribute].split(':')[0])
+  let hr = parseInt(item[attribute].split(':')[0])
   let min = parseInt(item[attribute].split(':')[1])
-  let startdate = dayjs(date)
-  if(starthr > 23){
-    startdate = startdate.add(1,'day')
-    starthr -= 24
-    //console.log(startdate + ' '+ starthr)
-  }
-  return dayjs(startdate).add(starthr, 'hours').add(min,'minute').utcOffset(9)
+  //let startdate = dayjs(date)
+  let isTomorrow = hr > 23
+  hr = isTomorrow ? hr-24 : hr
+  let dt = dayjs.tz(date+ ' '+hr+':'+min, 'Asia/Tokyo')
+  dt = isTomorrow? dt.add(1,'day'):dt
+
+  return dt
   //return dayjs(startdate + 'T'+item[attribute]+':00+09:00')
 }
